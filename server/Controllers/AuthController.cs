21201056bf602496ceeb277f8361f93693277437
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-04 01:58:42 -0300

    Added User Model and Auth Controller

---
+++ server/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server;
+
+[ApiController]
+[Route("auth")]
+public class AuthController : ControllerBase
+{
+    public AuthController()
+    {
+
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+    {
+
+    }
+
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequestDto loginRequestDto)
+    {
+
+    }
+}
---
+++ server/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace server;
+
+public class User : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Username { get; set; }
+    public string Email { get; set; }
+}
