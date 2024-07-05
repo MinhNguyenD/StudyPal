b93790a3f7ebdfa406951f458b1aed37b486e69a
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-05 12:31:47 -0300

    Set up navbar and router for register/login

--- client/src/router.ts
+++ client/src/router.ts
@@ -2,12 +2,15 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 import Contact from './components/contact/Contact.vue'
 import Homepage from './components/Homepage.vue'
 import FAQ from "./views/FAQ.vue"
+import Login from "./views/Login.vue"
+import Register from "./views/Register.vue"
+
 
 const routes = [
     { path: '/', component: Homepage },
     { path: '/faq', component: FAQ },
-    // { path: '/login', component: Login },
-    // { path: '/register', component: Register },
+    { path: '/login', component: Login },
+    { path: '/register', component: Register },
     { path: '/contact', component: Contact },
 ]
 
