d9600da1eaa5c02d41d51c7c0a7a8c1e836e27f3
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-21 01:13:16 -0300

    Implemented login frontend validation

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,51 +1,93 @@
 <template>
-    <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-            <!-- <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+  <div
+    class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8"
+  >
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <!-- <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
                 alt="StudyPal" /> -->
-            <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
-                account</h2>
+      <h2
+        class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <div
+        v-if="loginFail"
+        class="flex justify-between bg-red-300 text-red-800 p-5 mb-4 border-solid border-[1px] border-red-600 text-sm font-semibold"
+      >
+        <p>Incorrect username or password</p>
+        <button @click="() => (loginFail = false)" class="text-red-900">
+          &#x2715;
+        </button>
+      </div>
+      <form id="loginForm" @submit.prevent="handleSubmit" class="space-y-6">
+        <div>
+          <label
+            for="email"
+            class="block text-sm font-medium leading-6 text-gray-900"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              v-model="email"
+              id="email"
+              name="email"
+              type="email"
+              autocomplete="email"
+              required=""
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
 
-        <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form id="loginForm" @submit.prevent="handleSubmit" class="space-y-6">
-                <div>
-                    <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
-                    <div class="mt-2">
-                        <input v-model="email" id="email" name="email" type="email" autocomplete="email" required=""
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                </div>
-
-                <div>
-                    <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <!-- <div class="text-sm">
+        <div>
+          <div class="flex items-center justify-between">
+            <label
+              for="password"
+              class="block text-sm font-medium leading-6 text-gray-900"
+              >Password</label
+            >
+            <!-- <div class="text-sm">
                             <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
                         </div> -->
-                    </div>
-                    <div class="mt-2">
-                        <input v-model="password" id="password" name="password" type="password"
-                            autocomplete="current-password" required=""
-                            class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
-                    </div>
-                </div>
-
-                <div>
-                    <button type="submit"
-                        class="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign
-                        in</button>
-                </div>
-            </form>
-
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                {{ ' ' }}
-                <a href="#" class="font-semibold leading-6 text-primary hover:text-indigo-500">Create a new
-                    account</a>
-            </p>
+          </div>
+          <div class="mt-2">
+            <input
+              v-model="password"
+              id="password"
+              name="password"
+              type="password"
+              autocomplete="current-password"
+              required=""
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
         </div>
+
+        <div>
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-primary px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Sign in
+          </button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm text-gray-500">
+        Not a member?
+        {{ " " }}
+        <RouterLink
+          to="/register"
+          class="font-semibold leading-6 text-primary hover:text-indigo-500"
+        >
+          Create a new account
+        </RouterLink>
+      </p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -53,37 +95,34 @@ import { useUserStore } from "@/store/user";
 import axios from "axios";
 
 export default {
-    name: "Login",
-    setup() {
-        const userStore = useUserStore();
-        return { userStore };
+  name: "Login",
+  setup() {
+    const userStore = useUserStore();
+    return { userStore };
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      loginFail: false,
+    };
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const response = await axios.post("auth/login", {
+          email: this.email,
+          password: this.password,
+        });
+        localStorage.setItem("token", response.data.token);
+        this.userStore.login(response.data);
+        this.$router.push("/dashboard");
+      } catch (error) {
+        this.loginFail = true;
+        alert("Incorrect Username and/or Password!");
+      }
     },
-    data() {
-        return {
-            email: "",
-            password: "",
-        };
-    },
-    methods: {
-        async handleSubmit() {
-            try {
-                const response = await axios.post(
-                    "auth/login",
-                    {
-                        email: this.email,
-                        password: this.password,
-                    }
-                );
-                localStorage.setItem('token', response.data.token);
-                this.userStore.login(response.data);
-                //TODO: push to dashboard
-                this.$router.push("/dashboard");
-            } catch (error) {
-                console.log(error);
-                alert("Incorrect Username and/or Password!");
-            }
-        },
-    },
+  },
 };
 </script>
 
