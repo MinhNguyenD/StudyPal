3e9762047b3d359927b9c32771066220b7e2bd94
Lam Tieu <lm436150@dal.ca>
2024-07-26 19:37:07 -0300

    Fix type variable

--- client/src/views/UpdateSecurity.vue
+++ client/src/views/UpdateSecurity.vue
@@ -101,7 +101,7 @@ export default {
 
         },
         validateForm() {
-            const formValidated = true;
+            var formValidated = true;
             const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W){8,}/;
 
             if (!passwordRegex.test(this.newPassword)) {
