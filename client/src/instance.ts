e02a347d302eac4aa43877c0b90571f5bde03e22
Aryah Kannan <68197305+shamone03@users.noreply.github.com>
2024-07-26 20:47:40 -0300

    fixed deleted files by  git

--- client/src/instance.ts
+++ client/src/instance.ts
@@ -1,4 +1,10 @@
 import axios from "axios";
+// TODO: use authHeader() instead once state management is fixed
+const getHeader = () => {
+    return { "Authorization": "Bearer " + localStorage.getItem("token") }
+}
+
 export const server = axios.create({
-    baseURL: "http://localhost:5267/api/"
+    baseURL: "http://localhost:5267/api/",
+    headers: getHeader()
 });
