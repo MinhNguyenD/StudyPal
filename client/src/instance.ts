222c7bad599c37f08fa2f0ef7d4a57745a6c6f0d
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-07-27 02:09:33 -0300

    Changed to deployed url

--- client/src/instance.ts
+++ client/src/instance.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 // TODO: use authHeader() instead once state management is fixed
 const getHeader = () => {
-    return { "Authorization": "Bearer " + localStorage.getItem("token") }
-}
+  return { Authorization: "Bearer " + localStorage.getItem("token") };
+};
 
 export const server = axios.create({
-    // baseURL: "http://localhost:5267/api/",
-    baseURL: "https://csci4177grp-20-1.onrender.com/api",
-    headers: getHeader()
+  // baseURL: "http://localhost:5267/api/",
+  baseURL: "https://csci4177grp-20.onrender.com/",
+  headers: getHeader(),
 });
--- client/src/main.ts
+++ client/src/main.ts
@@ -5,6 +5,5 @@ import router from "./router";
 import axios from "axios";
 import { createPinia } from "pinia";
 
-axios.defaults.baseURL = "https://csci4177grp-20.onrender.com/";
 const pinia = createPinia();
 createApp(App).use(router).use(pinia).mount("#app");
