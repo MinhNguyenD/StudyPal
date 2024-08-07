6c18f32bb239981a80e3e9c64f1c8285dd2f249f
Ben Nguyen <nguyenducminh1307@gmail.com>
2024-08-07 16:01:29 -0300

    Fixed refreshed disappearing token error

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,10 +1,25 @@
-import { createApp } from "vue";
+import { createApp, watch } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
 import { createPinia } from "pinia";
+import { useUserStore } from "./store/user";
 
 axios.defaults.baseURL = "https://csci4177grp-20.onrender.com/";
 const pinia = createPinia();
-createApp(App).use(router).use(pinia).mount("#app");
+const app = createApp(App);
+app.use(router).use(pinia);
+const userStore = useUserStore();
+watch(
+    () => userStore.storedToken,
+    (newToken) => {
+        if (newToken) {
+            axios.defaults.headers.common['Authorization'] = 'Bearer ' + newToken;
+        } else {
+            delete axios.defaults.headers.common['Authorization'];
+        }
+    },
+    { immediate: true }
+);
+app.mount("#app");
